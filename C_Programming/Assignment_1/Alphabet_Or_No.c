e8cdb879923bd3cf25fb38ea9844a2b901c85c2b
Mahmoud Mowafey <mahmoodmowafey@gmail.com>
2022-12-12 22:25:51 +0300

    Trying to edit using vi comman

--- C_Programming/Assignment_1/Alphabet_Or_No.c
+++ C_Programming/Assignment_1/Alphabet_Or_No.c
@@ -1,1 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 5, 2022
+ *      Author: Mahmoud_Mowafey
+ */
 #include<stdio.h>
+
+int main(void)
+{
+	char x;
+	printf("Enter your token : \n");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%c",&x);
+	if( ( ( x >= 65 ) && ( x <= 90 ) ) || ( ( x >= 97 ) && ( x <= 122 ) ) )
+		printf("Alphabet Character\n");
+	else
+		printf("Not an Alphabet Character\n");
+	fflush(stdin);
+	fflush(stdout);
+}  
